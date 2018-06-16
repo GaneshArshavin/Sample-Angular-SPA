3ba9008f03ff683ecf242c12d641e8affcc4eccc
Ganesh Arshavin <r.ganesh@samsung.com>
2018-06-17 00:00:01 +0530

    Adding new chnges

--- app/app.js
+++ app/app.js
@@ -7,8 +7,14 @@ angular.module('myApp', [
   'myApp.view2',
   'myApp.version'
 ]).
-config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
-  $locationProvider.hashPrefix('!');
-
-  $routeProvider.otherwise({redirectTo: '/view1'});
+config(['$routeProvider', function( $routeProvider) {
+  $routeProvider.
+  when('/view1', {
+    templateUrl : 'view1/view1.html',
+    controller  : 'View1Ctrl'
+})
+. when('/view2', {
+  templateUrl : 'view2/view2.html',
+  controller  : 'View2Ctrl'
+})
 }]);
